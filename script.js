190de28a15c3c328f97ab58a5ec78636ef660c44
PetitB01 <baptiste.fouille@gmail.com>
2025-12-07 20:03:18 +0100

    JS fix

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
 const supabaseUrl = 'https://hzrmxqqdkeyhfwxtcfwf.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6cm14cXFka2V5aGZ3eHRjZndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwNTEyNjEsImV4cCI6MjA4MDYyNzI2MX0.OgSkI27XXexWxNUpxq33klPFSNOY7Gf1SPKnpeYs9WQ';
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+const { createClient } = await import('https://esm.sh/@supabase/supabase-js@2');
+
+window.supabase = createClient(supabaseUrl, supabaseKey);
 
 const form = document.getElementById('formulaire');
 const bouton = document.getElementById('envoyer');
 
 bouton.addEventListener('click', async (e) => {
 
-     e.preventDefault();
+    e.preventDefault();
     
     const first_name = form.prenom.value;
     const name = form.nom.value;
@@ -22,7 +23,7 @@ bouton.addEventListener('click', async (e) => {
 
     const remarques = form.commentaires.value;
 
-    const { data, error } = await supabase
+    const { data, error } = await window.supabase
         .from('NSI_formulaire')
         .insert([
             {
@@ -45,6 +46,7 @@ bouton.addEventListener('click', async (e) => {
         form.reset();
     }
 });
+
 
 
 

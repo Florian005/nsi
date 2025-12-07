5207fb19a3a54b02153a74d0e21b4a7a8de3ec3e
Florian005 <florian.wurger@caramail.fr>
2025-12-07 09:25:05 +0100

    enlevé l'ancien truc

--- script.js
+++ script.js
@@ -47,30 +47,3 @@ bouton.addEventListener('click', async (e) => {
 });
 
 
-
-
-
-
-
-
-
-//flo : (WIP)
-
-/*document.addEventListener("DOMContentLoaded", () => {
-    const bouton = document.getElementById("envoyer");
-    const formulaire = document.getElementById("formulaire");
-
-    bouton.addEventListener("click", (event) => {
-        event.preventDefault(); // empêche l'envoi réel du formulaire
-
-        // Récupération des valeurs
-        const prenom = formulaire.elements["prenom"].value;
-        const nom = formulaire.elements["nom"].value;
-        const email = formulaire.elements["email"].value;
-        const commentaires = formulaire.elements["commentaires"].value;
-    })
-})
-*/
-
-
-//tkt on peut le supprimer
